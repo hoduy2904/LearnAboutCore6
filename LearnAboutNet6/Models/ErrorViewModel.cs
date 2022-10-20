e412a0624c8550f77aebd683c340b264ed9e2a8b
S3CORP\duy.ho <hoduy2904@gmail.com>
2022-10-20 14:30:37 +0700

    Create Project, Install EF Core, Install Identity and Config

---
+++ LearnAboutNet6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnAboutNet6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
